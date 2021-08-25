d53f8303941727eaacbcd792e3a7c8223cae8b1d
Sungoh Park <spark449062@gmail.com>
2021-08-25 12:09:25 -0600

    weather and geocoding working

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import './App.css';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import GoogleMapReact from 'google-map-react';
 import {GOOGLE_KEY, OPENWEATHER_KEY} from './config.js'
 
 const newYorkCoord = { lat: 40.7128, lng: -74.0060 };
 const originCoord = newYorkCoord;
 
-// console.log(api.openweathermap.org/data/2.5/weather?q={Chicago}&appid={OPENWEATHER_KEY})
-
 function App() {
+  
+  // geocoding query
+  const address = "San Francisco, CA";
+  const geocodingAPI = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${GOOGLE_KEY}`;
+  useEffect(() => {
+    fetch(geocodingAPI)
+    .then(response => response.json())
+    .then(json => console.log(json))
+  }, []);
+
+  // weather query
+
+  const weatherAPI = `https://api.openweathermap.org/data/2.5/onecall?lat=${newYorkCoord.lat}&lon=${newYorkCoord.lng}&exclude=daily&appid=${OPENWEATHER_KEY}`;
+  useEffect(() => {
+    fetch(weatherAPI)
+    .then(response => response.json())
+    .then(json => console.log(json))
+  }, []);
+
+
+
+  
+
+  // map loading
   const mapOptions = {
     center: originCoord,
     zoom: 11
