fcf3401f96615f5cd85c1d923a279730bc820679
Sungoh Park <spark449062@gmail.com>
2021-08-25 10:54:49 -0600

    change to accomodate multiple API keys

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
-import API_KEY from './config.js'
+import {GOOGLE_KEY, OPENWEATHER_KEY} from './config.js'
 
 const newYorkCoord = { lat: 40.7128, lng: -74.0060 };
 const originCoord = newYorkCoord;
+
+// console.log(api.openweathermap.org/data/2.5/weather?q={Chicago}&appid={OPENWEATHER_KEY})
 
 function App() {
   const mapOptions = {
@@ -15,7 +17,7 @@ function App() {
     // Important! Always set the container height explicitly
     <div style={{ height: '100vh', width: '100%' }}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: API_KEY }}
+        bootstrapURLKeys={{ key: GOOGLE_KEY }}
         defaultCenter={mapOptions.center}
         defaultZoom={mapOptions.zoom}
       >
