45fe1bbf00cd96abaf6fa89b4d1b122d7a65ad05
Sungoh Park <spark449062@gmail.com>
2021-08-27 01:32:45 -0600

    display markers on search results

--- src/Map.js
+++ src/Map.js
@@ -9,64 +9,61 @@ function Map({address, addressLatLng}) {
   const newYorkCoord = { lat: 40.7128, lng: -74.0060 };
   const sanFranciscoCoord = {lat: 37.7749295, lng: -122.4194155}
   const originCoord = sanFranciscoCoord;
-  // map loading
+  // for initial map loading
   const mapOptions = {
     center: originCoord,
     zoom: 11
   };
 
-  const [mapApiLoaded, setMapApiLoaded] = useState(false)
-  const [mapInstance, setMapInstance] = useState(null)
-  const [mapApi, setMapApi] = useState(null)
+  // const [mapApiLoaded, setMapApiLoaded] = useState(false)
+  // const [mapInstance, setMapInstance] = useState(null)
+  // const [mapApi, setMapApi] = useState(null)
 
-  const handleApiLoaded = (map, maps) => {
-      setMapApiLoaded(true)
-      setMapInstance(map)
-      setMapApi(maps)
-// console.log(maps.places.PlacesService)
-      // console.log(mapApi)
+  function handleApiLoaded(map, maps) {
+    let request = {
+      location: addressLatLng,
+      rankBy: maps.places.RankBy.PROMINENCE,
+      keyword: "camping",
+      radius: "15000"
+    }
 
+    new maps.places.PlacesService(map).nearbySearch(request, nearbyCallback)
+    // console.log(map.center.lat)
+    // console.log(maps)
 
-
-      // new maps.Marker({
-      // position: addressLatLng,
-      // map
-      // })
+    function nearbyCallback(results, status) {
+      if (status == maps.places.PlacesServiceStatus.OK) {
+          console.log(results)
+          displayMarkers(results)
+      }
+    }
+    function displayMarkers(places) {
+      places.forEach(place => (
+        new maps.Marker({
+          position: place.geometry.location,
+          map: map,
+          title: place.name
+        })
+      ))
+    }
+    new maps.Marker({
+    position: addressLatLng,
+    map
+    })
     console.log("handleApiLoaded is called")
-  //   console.log(addressLatLng)
-
   };
+  
   return (
-      <div style={{ height: '100vh', width: '50%' }}>
-          <GoogleMapReact
-          bootstrapURLKeys={{ key: GOOGLE_KEY, libraries:['places', 'visualization']}}
-          defaultCenter={mapOptions.center}
-          center={addressLatLng}
-          defaultZoom={mapOptions.zoom}
-          yesIWantToUseGoogleMapApiInternals={true}
-          onGoogleApiLoaded={({ map, maps }) => {
-
-              let request = {
-                  location: addressLatLng,
-                  rankBy: maps.places.RankBy.DISTANCE,
-                  keyword: "ski"
-                }
-
-              let placeSearch = new maps.places.PlacesService(map).nearbySearch(request, nearbyCallback)
-              // console.log(map.center.lat)
-              // console.log(maps)
-
-              function nearbyCallback(results, status) {
-                  if (status == maps.places.PlacesServiceStatus.OK) {
-                      console.log(results)
-                  }
-              }
-
-              handleApiLoaded(map, maps)}
-          }
-          // onGoogleApiLoaded={({ map, maps }) => map.center(newYorkCoord)}
-          address={address}>
-          </GoogleMapReact>
+    <div style={{ height: '100vh', width: '50%' }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: GOOGLE_KEY, libraries:['places', 'visualization']}}
+        defaultCenter={mapOptions.center}
+        center={addressLatLng}
+        defaultZoom={mapOptions.zoom}
+        yesIWantToUseGoogleMapApiInternals={true}
+        onGoogleApiLoaded={({ map, maps }) => {handleApiLoaded(map, maps)}}
+        address={address}
+      />
     </div>
   )
 }
