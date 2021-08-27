f74f6b8c50db82e1a573c861ba32981dcacc4d67
Sungoh Park <spark449062@gmail.com>
2021-08-27 00:31:32 -0600

    conditional render on map due to restricted map re-render by google-map-react package

--- src/Map.js
+++ src/Map.js
@@ -6,69 +6,69 @@ const google = window.google;
 
 function Map({address, addressLatLng}) {
     
-    const newYorkCoord = { lat: 40.7128, lng: -74.0060 };
-    const sanFranciscoCoord = {lat: 37.7749295, lng: -122.4194155}
-    const originCoord = sanFranciscoCoord;
-    // map loading
-    const mapOptions = {
-      center: originCoord,
-      zoom: 11
-    };
+  const newYorkCoord = { lat: 40.7128, lng: -74.0060 };
+  const sanFranciscoCoord = {lat: 37.7749295, lng: -122.4194155}
+  const originCoord = sanFranciscoCoord;
+  // map loading
+  const mapOptions = {
+    center: originCoord,
+    zoom: 11
+  };
 
-    const [mapApiLoaded, setMapApiLoaded] = useState(false)
-    const [mapInstance, setMapInstance] = useState(null)
-    const [mapApi, setMapApi] = useState(null)
+  const [mapApiLoaded, setMapApiLoaded] = useState(false)
+  const [mapInstance, setMapInstance] = useState(null)
+  const [mapApi, setMapApi] = useState(null)
 
-    const handleApiLoaded = (map, maps) => {
-        setMapApiLoaded(true)
-        setMapInstance(map)
-        setMapApi(maps)
+  const handleApiLoaded = (map, maps) => {
+      setMapApiLoaded(true)
+      setMapInstance(map)
+      setMapApi(maps)
 // console.log(maps.places.PlacesService)
-        // console.log(mapApi)
+      // console.log(mapApi)
 
-  
 
-        // new maps.Marker({
-        // position: addressLatLng,
-        // map
-        // })
-      console.log("handleApiLoaded is called")
-    //   console.log(addressLatLng)
 
-    };
-    return (
-        <div style={{ height: '100vh', width: '50%' }}>
-            <GoogleMapReact
-            bootstrapURLKeys={{ key: GOOGLE_KEY, libraries:['places', 'visualization']}}
-            defaultCenter={mapOptions.center}
-            center={addressLatLng}
-            defaultZoom={mapOptions.zoom}
-            yesIWantToUseGoogleMapApiInternals={true}
-            onGoogleApiLoaded={({ map, maps }) => {
+      // new maps.Marker({
+      // position: addressLatLng,
+      // map
+      // })
+    console.log("handleApiLoaded is called")
+  //   console.log(addressLatLng)
 
-                let request = {
-                    location: addressLatLng,
-                    rankBy: maps.places.RankBy.DISTANCE,
-                    keyword: "ski"
-                  }
+  };
+  return (
+      <div style={{ height: '100vh', width: '50%' }}>
+          <GoogleMapReact
+          bootstrapURLKeys={{ key: GOOGLE_KEY, libraries:['places', 'visualization']}}
+          defaultCenter={mapOptions.center}
+          center={addressLatLng}
+          defaultZoom={mapOptions.zoom}
+          yesIWantToUseGoogleMapApiInternals={true}
+          onGoogleApiLoaded={({ map, maps }) => {
 
-                let placeSearch = new maps.places.PlacesService(map).nearbySearch(request, nearbyCallback)
-                // console.log(map.center.lat)
-                // console.log(maps)
-
-                function nearbyCallback(results, status) {
-                    if (status == maps.places.PlacesServiceStatus.OK) {
-                        console.log(results)
-                    }
+              let request = {
+                  location: addressLatLng,
+                  rankBy: maps.places.RankBy.DISTANCE,
+                  keyword: "ski"
                 }
 
-                handleApiLoaded(map, maps)}
-            }
-            // onGoogleApiLoaded={({ map, maps }) => map.center(newYorkCoord)}
-            address={address}>
-            </GoogleMapReact>
-      </div>
-    )
+              let placeSearch = new maps.places.PlacesService(map).nearbySearch(request, nearbyCallback)
+              // console.log(map.center.lat)
+              // console.log(maps)
+
+              function nearbyCallback(results, status) {
+                  if (status == maps.places.PlacesServiceStatus.OK) {
+                      console.log(results)
+                  }
+              }
+
+              handleApiLoaded(map, maps)}
+          }
+          // onGoogleApiLoaded={({ map, maps }) => map.center(newYorkCoord)}
+          address={address}>
+          </GoogleMapReact>
+    </div>
+  )
 }
 
 export default Map;
