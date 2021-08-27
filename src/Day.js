736a210a8d1ad8ed9966063323a5470427dd0d45
Kayla Phelps <kmphelps822@gmail.com>
2021-08-26 19:18:51 -0700

    display weather images

--- src/Day.js
+++ src/Day.js
@@ -1,10 +1,12 @@
-import React from "react";
-
+import React, {useState} from "react";
+import cloudy from "./images/cloudy.png";
+import rainy from "./images/rainy.png";
+import snow from "./images/snow.png";
+import sunny from "./images/sunny.png";
 
 function Day({day}) {
     const {temp} = day;
     const date = new Date(day.dt * 1000).toLocaleString();
-    
 
     function kelvinToCelcius (tempKelvin) {
         return (tempKelvin - 273.15).toFixed(1);
@@ -14,12 +16,28 @@ function Day({day}) {
         return ((tempKelvin - 273.15)*(9/5) + 32).toFixed(1);
     }
 
-   
+    function handleWeatherImage(weather){
+        if (weather === "Clouds") {
+            return cloudy
+           
+        } else if (weather === "Clear") {
+            return sunny
+        
+        } else if (weather === "Rain") {
+            return rainy
+        
+        } else if (weather === "Snow") {
+            return snow
+        
+        } else {return sunny}
+    }
 
+   
     return (
         <div> 
         {!day ? "..." : 
             <div>
+                <img src={handleWeatherImage(day.weather[0].main)}></img>
                 <p> {date} </p>
                 <p> {day.weather[0].main} </p>
                 <p> {kelvinToFahrenheit(temp.day) + "°F / " + kelvinToCelcius(temp.day) + "°C"}</p>
